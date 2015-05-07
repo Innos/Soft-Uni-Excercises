14f9dbd3f66809b10ad8345b8f53a8530de43868
Innos <victor.d.kazakov@gmail.com>
2015-05-08 01:54:53 +0300

    added excercises

---
+++ CSharp Advanced/CSharp Advanced Excercise 1/CSharp Advanced Excercise 1/01SortUsingBubbleSort/SortUsingBubbleSort.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class SortUsingBubbleSort
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+        BubbleSort(numbers);
+        foreach (var item in numbers)
+        {
+            Console.WriteLine(item);
+        }
+    }
+    static void BubbleSort(int[] numbers)
+    {
+        bool swapped = false;
+
+        do
+        {
+            swapped = false;
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                if (numbers[i] > numbers[i + 1])
+                {
+                    int swap = numbers[i];
+                    numbers[i] = numbers[i + 1];
+                    numbers[i + 1] = swap;
+                    swapped = true;
+                }
+            }
+
+        } while (swapped == true);
+    }
+}
+
---
+++ CSharp Advanced/CSharp Advanced Excercise 1/CSharp Advanced Excercise 1/02JoinLists/JoinLists.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class JoinLists
+{
+    static void Main(string[] args)
+    {
+        int[] input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+        List<int> list1 = new List<int>();
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (!list1.Contains(input[i]))
+            {
+                list1.Add(input[i]);
+            }
+        }
+        input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (!list1.Contains(input[i]))
+            {
+                list1.Add(input[i]);
+            }
+        }
+        list1.Sort();
+        list1.ForEach(i => Console.Write(i + " "));
+    }
+}
+

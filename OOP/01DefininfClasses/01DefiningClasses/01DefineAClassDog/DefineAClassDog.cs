22deb88366ebc78de27c9e994412ce20fb4e116e
Innos <victor.d.kazakov@gmail.com>
2015-06-02 22:05:21 +0300

    added OOP excercise 01

---
+++ OOP/01DefininfClasses/01DefiningClasses/01DefineAClassDog/DefineAClassDog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class DefineAClassDog
+{
+    static void Main(string[] args)
+    {
+        Dog sharo = new Dog("sharo", "melez");
+        Dog unnamed = new Dog();
+        Dog roger = new Dog("Roger", "Golden Retriever");
+
+        unnamed.Breed = "German Shepherd";
+
+        sharo.Bark();
+        unnamed.Bark();
+        roger.Bark();
+    }
+}
+
---
+++ OOP/01DefininfClasses/01DefiningClasses/01DefineAClassDog/Dog.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+
+class Dog
+{
+    public string Name { get; set; }
+    public string Breed { get; set; }
+
+    public Dog() : this(null,null)
+    {
+    }
+    public Dog(string name,string breed)
+    {
+        this.Name = name;
+        this.Breed = breed;
+    }
+
+    public void Bark()
+    {
+        Console.WriteLine("{0} ({1}) said: Bauuuuuuu!",
+            this.Name ?? "[unnamed dog]",
+            this.Breed ?? "[unknown breed]");
+    }
+}
+

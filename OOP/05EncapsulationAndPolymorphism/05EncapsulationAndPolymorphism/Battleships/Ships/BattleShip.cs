1a541db9bf57b1efbbb7fdcde31bcf620bf235e0
Innos <victor.d.kazakov@gmail.com>
2015-06-10 15:21:46 +0300

    added excercise 5

---
+++ OOP/05EncapsulationAndPolymorphism/05EncapsulationAndPolymorphism/Battleships/Ships/BattleShip.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Battleships.Interface;
+
+namespace Battleships.Ships
+{
+    public abstract class BattleShip : Ship,IAttack
+    {
+        public BattleShip(string name, double lengthInMeters, double volume) : base(name,lengthInMeters,volume)
+        {
+        }
+
+        public abstract string Attack(Ship targetShip);
+
+        public void DestroyShip(Ship target)
+        {
+            target.IsDestroyed = true;
+        }
+    }
+}
---
+++ OOP/05EncapsulationAndPolymorphism/05EncapsulationAndPolymorphism/BookStore/BookStoreMain.cs
@@ -0,0 +1,17 @@
+﻿namespace BookStore
+{
+    using Engine;
+    using BookStore.UI;
+
+    public class BookStoreMain
+    {
+        public static void Main()
+        {
+            ConsoleInputHandler iHandler = new ConsoleInputHandler();
+            ConsoleRenderer renderer = new ConsoleRenderer();
+            BookStoreEngine engine = new BookStoreEngine(renderer,iHandler);
+
+            engine.Run();
+        }
+    }
+}

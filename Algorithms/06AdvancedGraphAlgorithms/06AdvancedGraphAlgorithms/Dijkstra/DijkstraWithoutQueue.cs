70e3671761434a865b57fe308dded8830f8df64f
Innos <victor.d.kazakov@gmail.com>
2015-11-15 23:51:42 +0200

    added dijkstra

--- Algorithms/06AdvancedGraphAlgorithms/06AdvancedGraphAlgorithms/Dijkstra/DijkstraWithoutQueue.cs
+++ Algorithms/06AdvancedGraphAlgorithms/06AdvancedGraphAlgorithms/Dijkstra/DijkstraWithoutQueue.cs
@@ -7,7 +7,74 @@
     {
         public static List<int> DijkstraAlgorithm(int[,] graph, int sourceNode, int destinationNode)
         {
-            throw new NotImplementedException();
+            int n = graph.GetLength(0);
+            int[] distance = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                distance[i] = int.MaxValue;
+            }
+            distance[sourceNode] = 0;
+
+            int?[] previous = new int?[n];
+            bool[] used = new bool[n];        
+
+            Dijkstra(used,distance,graph,previous);
+            return ReconstructSequence(distance, previous, destinationNode);
+        }
+
+        private static void Dijkstra(bool[] used, int[] distance, int[,] graph, int?[] previous)
+        {
+            while (true)
+            {
+                int minDistance = int.MaxValue;
+                int minNode = 0;
+                for (int node = 0; node < distance.Length; node++)
+                {
+                    if (!used[node] && distance[node] < minDistance)
+                    {
+                        minDistance = distance[node];
+                        minNode = node;
+                    }
+                }
+
+                if (minDistance == int.MaxValue)
+                {
+                    break;
+                }
+                used[minNode] = true;
+
+                for (int i = 0; i < distance.Length; i++)
+                {
+                    if (graph[minNode, i] > 0)
+                    {
+                        int newDistance = distance[minNode] + graph[minNode, i];
+                        if (newDistance < distance[i])
+                        {
+                            distance[i] = newDistance;
+                            previous[i] = minNode;
+                        }
+                    }
+                }
+            }
+        }
+
+        private static List<int> ReconstructSequence(int[] distance, int?[] previous, int destinationNode)
+        {
+            if (distance[destinationNode] == int.MaxValue)
+            {
+                return null;
+            }
+
+            List<int> path = new List<int>();
+            int? currentNode = destinationNode;
+            while (currentNode != null)
+            {
+                path.Add(currentNode.Value);
+                currentNode = previous[currentNode.Value];
+            }
+
+            path.Reverse();
+            return path;
         }
     }
 }

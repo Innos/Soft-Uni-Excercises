8db5861663e89653f527192b31781f8455aa8dc4
Innos <victor.d.kazakov@gmail.com>
2016-03-14 06:12:29 +0200

    added AVL Lab

---
+++ DataStructures/08AVLAndBinaryHeap/8. AvlTree-Skeleton/AvlTreeLab/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace AvlTreeLab
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var nums = new int[] { 1, 2, 3, 5, 13, 22 };
+
+            var tree = new AvlTree<int>();
+            foreach (int num in nums)
+            {
+                tree.Add(num);
+            }
+
+            tree.Remove(3);
+            tree.Remove(13);
+            tree.Remove(1);
+            tree.Add(4);
+
+            foreach (var node in tree)
+            {
+                Console.WriteLine(node);
+            }
+        }
+    }
+}
